10f79a54bc9dfa4c3b7756d2cc0117065fdd73ed
welmahjoub <mahjoubwel@gmail.com>
2020-02-13 10:41:34 +0100

    creation  service api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiServiceService} from './api-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'sirFront';
+  constructor(private api: ApiServiceService) {
+  }
+  ngOnInit(): void {
+    this.api.getListeUsers().subscribe(res => {
+      this.title = res;
+    });
+  }
 }
